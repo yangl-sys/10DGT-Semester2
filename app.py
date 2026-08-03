b5279539cd902aaa8dcbe4e91594a177c7fdea00
yangl-sys <yangl@pedarecc.sa.edu.au>
2026-08-03 13:04:02 +0930

    connected data base to app

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import sqlite3
 
 app = Flask(__name__)
 
+
+def get_db_connection():
+    conn = sqlite3.connect('pedair.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_all_cities():
+    # Fetches a clean, sorted list of all unique origin cities in the database.
+    conn = get_db_connection()
+    cities_query = 'SELECT DISTINCT origin FROM flights ORDER BY origin ASC'
+    
+    # Extract the string value from each row row['origin']
+    db_cities = [row['origin'] for row in  
+    conn.execute(cities_query).fetchall()]
+    conn.close()
+    return db_cities
+
+
 @app.route('/')
 def index():
-    # START CODE: Currently hardcoded. 
-    # TASK: Students will eventually fetch this from SQLite/Faker.
-    sample_flights = [
-        {"id": 1, "origin": "Adelaide", "dest": "Melbourne", "date": "2024-11-12", "price": 89, "time": "14:20"},
-        {"id": 2, "origin": "Adelaide", "dest": "Sydney", "date": "2024-11-12", "price": 124, "time": "10:05"},
-        {"id": 3, "origin": "Adelaide", "dest": "Gold Coast", "date": "2024-11-13", "price": 156, "time": "08:30"}
-    ]
-    return render_template('index.html', flights=sample_flights)
+    conn = get_db_connection()
+    db_flights = conn.execute('SELECT * FROM flights').fetchall()
+    conn.close()
+    return render_template('index.html', flights=db_flights)
 
 if __name__ == '__main__':
     app.run(debug=True)
